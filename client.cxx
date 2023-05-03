135d926c024799817046c79fa0c8e248f917995a
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-03 12:14:56 +0500

    Указание IP-адреса сервера

--- client.cxx
+++ client.cxx
@@ -6,7 +6,7 @@ main(int nargs, char* args[]) {
   using namespace boost;
   asio::io_context context{};
 
-  const auto server_addr{asio::ip::make_address("127.0.0.1")};
+  const auto server_addr{asio::ip::make_address("192.168.0.10")};
   asio::ip::udp::endpoint endpoint{server_addr, 3333u};
   asio::ip::udp::socket socket{context};
 
